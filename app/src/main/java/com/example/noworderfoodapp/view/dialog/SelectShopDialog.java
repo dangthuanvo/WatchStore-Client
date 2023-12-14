824307fc2255aa3a755f765d34660cf2dd3ed7bb
Phú Túc <102684682+phutuctran@users.noreply.github.com>
2023-12-14 23:42:25 +0700

    update filteroption

---
+++ app/src/main/java/com/example/noworderfoodapp/view/dialog/SelectShopDialog.java
@@ -0,0 +1,78 @@
+package com.example.noworderfoodapp.view.dialog;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.noworderfoodapp.R;
+import com.example.noworderfoodapp.entity.Shop;
+import com.example.noworderfoodapp.view.adapter.ShopAdapter;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class SelectShopDialog extends DialogFragment implements ShopAdapter.OnItemClick {
+
+    public static String TAG = SelectShopDialog.class.getName();
+    private RecyclerView rcvShop;
+    private ShopAdapter shopAdapter;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            getDialog().getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_select_shop, container);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rcvShop = view.findViewById(R.id.rcv_shop);
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            // Lấy danh sách sản phẩm từ bundle
+            Serializable serializable = bundle.getSerializable("shopList");
+            if (serializable instanceof ArrayList) {
+                ArrayList<Shop> productList = (ArrayList<Shop>) serializable;
+                shopAdapter = new ShopAdapter(productList,getContext());
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+                rcvShop.setLayoutManager(linearLayoutManager);
+                rcvShop.setAdapter(shopAdapter);
+                shopAdapter.setOnItemClick(this);
+            }
+        }
+    }
+    private OnItemClick callBack;
+
+    public void setOnItemClick(OnItemClick event) {
+        callBack = event;
+    }
+
+    @Override
+    public void onItemClick(Shop shop) {
+
+    }
+
+    @Override
+    public void onFavoriteShopClick(Shop shop) {
+
+    }
+
+    public interface OnItemClick {
+        void onItemClick(String key);
+    }
+}
